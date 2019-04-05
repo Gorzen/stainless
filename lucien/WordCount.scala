2586abc9fc1746b79300de0964bbbb7dce17b908
Lucien Iseli <lucien.iseli93@gmail.com>
2019-04-05 14:52:45 +0200

    Modify WordCount to use non-generic map

--- lucien/WordCount.scala
+++ lucien/WordCount.scala
@@ -6,61 +6,26 @@ object WordCount {
   import MonoidLaws._
   import MonoidMap._
 
-  // How to use stainless's Map?
-  // It doesn't have map and it doesn't have postconditions
-  case class WC(words: MMap[String, BigInt])
+  case class WC(words: MMap)
 
-  case class WordCountMonoid(monoid: Monoid[BigInt]) extends Monoid[WC] {
-    //implicit val monoid = M
+  case class WordCountMonoid() extends Monoid[WC] {
+    def empty: WC = WC(MMap.empty)
 
-    def empty: WC = WC(MMap.empty(monoid))
+    def append(x: WC, y: WC): WC = WC(x.words.merge(y.words))
 
-    def append(x: WC, y: WC): WC = WC(x.words.merge(y.words)(monoid))
-
-    /*def lemma_leftIdentity(x: WC): Boolean = {
-      (append(empty, x) == x) because {
-        assert(append(empty, x) == append(WC(MMap.empty), x))
-        assert(append(WC(MMap.empty), x) == WC(MMap.empty.merge(x.words)))
-        assert(WC(MMap.empty.merge(x.words)) == WC(MMap{ k =>
-          M.append(MMap.empty.apply(k), x.words.apply(k))
-        }))
-        assert(WC(MMap{ k =>
-          M.append(MMap.empty.apply(k), x.words.apply(k))
-        }) == WC(MMap{ k =>
-          M.append(M.empty, x.words.apply(k))
-        }))
-        assert(WC(MMap{ k =>
-          M.append(M.empty, x.words.apply(k))
-        }) == WC(MMap{ k =>
-          M.law_leftIdentity(x.words.apply(k))
-          M.append(M.empty, x.words.apply(k))
-        }))
-        assert(WC(MMap{ k =>
-          M.law_leftIdentity(x.words.apply(k))
-          M.append(M.empty, x.words.apply(k))
-        }) == WC(MMap{ k =>
-          x.words.apply(k)
-        }))
-        assert(WC(MMap{ k =>
-          x.words.apply(k)
-        }) == WC(x.words))
-        assert(WC(x.words) == x)
-        check(append(empty, x) == x)
-      }
-    }*/
-
-    def lemma_rightIdentity(x: WC): Boolean = {
-      (append(empty, x) == x) because {
-        // fairly easy, y.words.map will still be empty map -> you get x.words
-        true
-      }
+    @library
+    override def law_leftIdentity(x: WC) = {
+      super.law_leftIdentity(x) because true
     }
 
-    def lemma_associativity(x: WC, y: WC, z: WC): Boolean = {
-      (append(x, append(y, z)) == append(append(x, y), z)) because {
-        // Doesn't look easy...
-        true
-      }
+    @library
+    override def law_rightIdentity(x: WC) = {
+      super.law_rightIdentity(x) because true
+    }
+
+    @library
+    override def law_associativity(x: WC, y: WC, z: WC) = {
+      super.law_associativity(x, y, z) because true
     }
   }
 }
